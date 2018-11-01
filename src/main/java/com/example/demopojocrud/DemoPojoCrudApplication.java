4ba4e80543b1fe0c2e674f52c16be8815e33cc46
Chen Boly <chenboly@gmail.com>
2018-11-01 13:41:15 +0700

    Initial commit

---
+++ src/main/java/com/example/demopojocrud/DemoPojoCrudApplication.java
@@ -0,0 +1,12 @@
+package com.example.demopojocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPojoCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoPojoCrudApplication.class, args);
+    }
+}
